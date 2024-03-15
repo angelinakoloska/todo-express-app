dcafd4bb5116300fae69c2d2632803fbf9e35929
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-03-15 09:18:53 +0100

    Add user service

---
+++ services/UserService.js
@@ -0,0 +1,35 @@
+const { Op } = require("sequelize");
+
+class UserService {
+  constructor(db) {
+    this.client = db.sequelize;
+    this.User = db.User;
+  }
+
+  async create(email, password) {
+    return this.User.create({
+      Email: email,
+      Password: password
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  async getAll() {
+    return this.User.findAll({
+      where: {}
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+
+  async getOneByEmail(email) { 
+    return await this.User.findOne({
+      where: {Email: email}
+    }).catch(function (err) {
+      console.log(err);
+    });
+  }
+}
+
+module.exports = UserService;
